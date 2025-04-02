61f935066a93697a0e052198f70fd4ece2babb05
Kaiizer <haniamiine@gmail.com>
2025-04-02 14:34:27 +0000

    remove useless code

--- src/pages/Products/ProductPage.tsx
+++ src/pages/Products/ProductPage.tsx
@@ -6,43 +6,6 @@ import { sendFromWebhook } from '../../service/plateforms/discord'
 import { useGetProductsQuery } from '../../service/products'
 import Loader from '../../components/UI/Loader/Loader'
 
-// const products = [
-//   {
-//     title: 'Kit Moteur',
-//     description: "Un ensemble complet de composants pour l'assemblage et la maintenance de moteurs haute performance.",
-//     price: 500,
-//     img: 'https://png.pngtree.com/png-vector/20240731/ourmid/pngtree-mechanic-s-toolbox-png-image_13308791.png',
-//   },
-//   {
-//     title: 'Acier',
-//     description: 'Un matériau robuste et polyvalent, idéal pour la construction et la fabrication de quincaillerie.',
-//     price: 45,
-//     img: 'https://satisfactory.wiki.gg/images/4/48/Aluminum_Ingot.png',
-//   },
-//   {
-//     title: 'Cuivre',
-//     description: 'Un métal conducteur de haute qualité, parfait pour les applications électriques et thermiques.',
-//     price: 55,
-//     img: 'https://satisfactory.wiki.gg/images/0/00/Copper_Ingot.png',
-//   },
-//   {
-//     title: 'Acier Compacté',
-//     description:
-//       "Une version renforcée de l'acier standard, offrant une résistance accrue et une meilleure durabilité.",
-//     price: 100,
-//     img: 'https://leanwaste.fr/wp-content/uploads/2022/09/toles.png',
-//   },
-//   {
-//     title: "Lingot d'Or",
-//     description: 'Un produit exclusif et hautement confidentiel. Manipulation restreinte et discrétion recommandée.',
-//     price: 1000,
-//     stock: 0,
-//     disabled: true,
-//     show: false,
-//     img: 'https://static.vecteezy.com/system/resources/previews/016/659/386/non_2x/shiny-gold-bars-free-png.png',
-//   },
-// ]
-
 interface CartItem {
   item: string
   qte: number
